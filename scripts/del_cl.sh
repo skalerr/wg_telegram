1fdaaa243ee60a10ce1fde58392c9b7b0e81ac2f
Robert <rob06121@gmail.com>
2025-08-27 03:40:11 +0500

    test new config

--- scripts/del_cl.sh
+++ scripts/del_cl.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 ip=$1
-my_variable=$(grep -n "$wg_local_ip_hint.${ip}" /etc/wireguard/wg0.conf | cut -d ':' -f 1)
+my_variable=$(grep -n "AllowedIPs = $wg_local_ip_hint.${ip}/32" /etc/wireguard/wg0.conf | cut -d ':' -f 1)
+
+if [ -z "$my_variable" ]; then
+    echo "Client IP $wg_local_ip_hint.${ip} not found"
+    exit 1
+fi
 
 sed -i "$(($my_variable-2)),$my_variable d" /etc/wireguard/wg0.conf
 #systemctl restart wg-quick@wg0
