c183e8e29d2a142c0386790388225ad9034b10a3
Robert <rob06121@gmail.com>
2025-08-27 02:21:09 +0500

    fix problem with del_cl

--- scripts/del_cl.sh
+++ scripts/del_cl.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+source scripts/variables.sh
+
 ip=$1
 my_variable=$(grep -n "$wg_local_ip_hint.${ip}" /etc/wireguard/wg0.conf | cut -d ':' -f 1)
+
+if [ -z "$my_variable" ]; then
+    echo "IP адрес $wg_local_ip_hint.$ip не найден в конфигурации"
+    exit 1
+fi
 
 sed -i "$(($my_variable-2)),$my_variable d" /etc/wireguard/wg0.conf
 #systemctl restart wg-quick@wg0
